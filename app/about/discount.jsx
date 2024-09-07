7ea85300b36f81bb471e7199b360c89923c928d8
Bwise1 <oguntoyebenjamin2@gmail.com>
2024-09-07 15:04:38 +0300

    Style: finished responsive about

--- app/about/discount.jsx
+++ app/about/discount.jsx
@@ -1,14 +1,23 @@
-
 import Image from "next/image";
 import Link from "next/link";
 
-
 export default function Discount() {
   return (
-    <section className="w-full  lg:h-[381px] bg-aboutBg2">
-      <div className="relative  bg-[#F0712766] h-full w-full flex flex-col items-center lg:py-12 py-10  ">
-    <div className="mx-auto pt-2 font-bold text-white lg:text-[50px] xl:text-[54px] lg:w-[690px] xl:w-[750px] text-center ">Get 15% off your first appointment with Cerrene!</div> 
-    <Link href='/contact' className="h-[60px] mt-9 font-satoshi font-bold text-white  w-[277px] bg-cerreneOrange flex items-center justify-center rounded">Contact Us</Link> 
+    <section className="w-full slg:min-h-[381px] bg-aboutBg2">
+      <div className="relative bg-[#F0712766] h-full w-full flex flex-col items-center slg:py-12 py-10">
+        <div
+          className="mx-auto pt-2 font-bold text-white text-center
+        text-[28px] md:text-[36px] slg:text-[50px] xl:text-[54px]
+        w-[90%] md:w-[80%] slg:w-[690px] xl:w-[750px]"
+        >
+          Get 15% off your first appointment with Cerrene!
+        </div>
+        <Link
+          href="/contact"
+          className="h-[50px] md:h-[60px] mt-6 md:mt-8 font-satoshi font-bold text-white w-[220px] md:w-[277px] bg-cerreneOrange flex items-center justify-center rounded"
+        >
+          Contact Us
+        </Link>
       </div>
     </section>
   );
